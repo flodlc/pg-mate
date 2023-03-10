a470767559575c97106c25e8c2c193dbac5544dc
flodlc <f.delacomble@gmail.com>
2023-03-10 21:57:31 +0100

    clean the playground migration

--- playground/migrations/migration_1677869240309.ts
+++ playground/migrations/migration_1677869240309.ts
@@ -1,16 +1,5 @@
 import { Client } from "pg";
 
-export const up = async (pg: Client) => {
-  pg.query(`
-        CREATE TABLE users(
-            id int PRIMARY KEY,
-            name varchar NOT NULL
-        );
-    `);
-};
+export const up = async () => {};
 
-export const down = async (pg: Client) => {
-  pg.query(`
-        DROP TABLE users;
-    `);
-};
+export const down = async () => {};
